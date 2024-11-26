ca628f8866f066cc92e7bcacc4cb4b55edef3584
Anderson <andersonrw16@hotmail.com>
2024-11-26 10:48:20 -0300

    refactor(): update project folder structure

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use(routes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 const PORT = 3000;
